b4782486f31a311562748afdd2a43dfadb5ec39c
huna <huna@wemakeprice.com>
2019-07-19 17:26:17 +0900

    Create the entry point for ExpressJS

---
+++ meadowlark.js
@@ -0,0 +1,22 @@
+const express = require('express');
+
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+// custom 404 page
+app.use((req, res) => {
+  res.type('text/plain');
+  res.status(400).send('404 - Not found');
+});
+
+// custom 500 page
+app.use((err, req, res, next) => {
+  console.error(err.message);
+  res.type('text/plain');
+  res.status(500).send('500 - Server Error');
+});
+
+app.listen(port, () => console.log(
+  `Express started on http://localhost:${port}\npress Ctrl-C to terminate.`
+));
