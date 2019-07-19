ba2157c98a09bec25843c0c609f303d14a15c2d9
huna <huna@wemakeprice.com>
2019-07-19 17:44:06 +0900

    Applied view engine - handlebars

--- meadowlark.js
+++ meadowlark.js
@@ -1,30 +1,28 @@
 const express = require('express');
+const expressHandlebars = require('express-handlebars');
 
 const app = express();
 
+app.engine('handlebars', expressHandlebars({
+  defaultLayout: 'main'
+}));
+app.set('view engine', 'handlebars');
+
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  res.type('text/plain');
-  res.send('Meadowlark Travel');
-});
+app.get('/', (req, res) => res.render('home'));
 
-app.get('/about', (req, res) => {
-  res.type('text/plain');
-  res.send('About Meadowlark Travel');
-});
+app.get('/about', (req, res) => res.render('about'));
 
 // custom 404 page
 app.use((req, res) => {
-  res.type('text/plain');
-  res.status(400).send('404 - Not found');
+  res.status(400).render('404');
 });
 
 // custom 500 page
 app.use((err, req, res, next) => {
   console.error(err.message);
-  res.type('text/plain');
-  res.status(500).send('500 - Server Error');
+  res.status(500).render('500');
 });
 
 app.listen(port, () => console.log(
